4f0a878dcd1a6ff1fa1cc20645f175b813697c37
HandMeTheCode <cristian.andrade@utp.ac.pa>
2023-10-26 12:34:52 -0500

    modifying edit.js, adding styles.css file

--- LAB7/edit.js
+++ LAB7/edit.js
@@ -1,4 +1,49 @@
-//function myfunction(){
-//var x = document.getElementById(parrafo);
-//x.style.color="red";}
+// function changeColor(){
+//     var x = document.getElementById("parrafo");
+//     x.style.color="red";
+//   }
+//   function addText() {
+//       var sentenceElement = document.getElementById("sentence");
+//       var currentText = sentenceElement.textContent;
+//       var wordToAdd = ", palabra adicional";
+//       sentenceElement.textContent = currentText + wordToAdd;
+//   }
+//   function concatenateText() {
+//         var text1 = " Curva ";
+//         var text2 = "de velocidad";
+//         document.getElementById("Concatenar").textContent = text2 + text1;
+//   }
+//   function removeOption() {
+//         var dropdown = document.getElementById("dropdown");
+//         var valueToRemove = "Animacion de fotogramas clave";
 
+//         for (var i = 0; i < dropdown.options.length; i++) {
+//             if (dropdown.options[i].value === valueToRemove) {
+//                 dropdown.remove(i);
+//                 break;
+//             }
+//         }
+//     }
+//     function redirectToYouTube() {
+//         var youtubeVideoURL = "https://www.youtube.com/watch?v=VIDEO_ID";
+//         window.location.href = youtubeVideoURL;
+//     }
+//     function addErrorText() {
+//       var sentenceElement = document.getElementById("errorSentence");
+//       var currentText = sentenceElement.textContent;
+//       var wordToAdd = ", haz click al boton de ejemplo";
+//       sentenceElement.textContent = currentText + wordToAdd;
+//     }
+//     function sendMessage() {
+//         var message = document.getElementById("Concatenar").textContent;
+//         alert(message);
+//     }
+//     function printName() {
+//         var name = "Johnny Deep"; // Replace with the name you want to print
+//         console.log("Name: " + name);
+//     }
+//     function checkStatus() {
+//         var checkbox = document.getElementById("myCheckbox");
+//         var status = checkbox.checked ? "Checked" : "Unchecked";
+//         alert("Checkbox Status: " + status);
+//     }
